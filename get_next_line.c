4b58f87dbeef82e177f967db42f3f48f034e40fc
Ana Novaes <ana.novaes@cloudwalk.io>
2021-05-15 18:39:00 -0300

    revertendo fix de protecao temp

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,15 @@
 /*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 19:50:53 by apaula-b          #+#    #+#             */
-/*   Updated: 2021/05/15 18:33:33 by apaula-b         ###   ########.fr       */
+/*   Updated: 2021/05/15 18:38:38 by apaula-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 #include "get_next_line.h"
+
+
 
 void	*ft_calloc(size_t count, size_t size)
 {
@@ -40,14 +42,10 @@ static char	*new_line(char **next_line, char **temp)
 {
 	int		end_line;
 	char	*new_temp;
-	int		size_temp;
 
-	size_temp = ft_strlen(*temp);
 	new_temp = NULL;
 	end_line = ft_strchr(*temp, '\n');
-	if (end_line == -1)
-		end_line = ft_strchr(*temp, '\0');
-	if (size_temp > end_line)
+	if (end_line != -1)
 	{
 		temp[0][end_line] = '\0';
 		*next_line = ft_strdup(*temp);
@@ -66,10 +64,13 @@ static char	*new_line(char **next_line, char **temp)
 int	read_file(int fd, char *buff, char **temp, int *b_read)
 {
 	char	*temp_buff;
+	int		verifier;
 
+	verifier = 0;
 	while (*b_read && (ft_strchr(*temp, '\n') == -1
 			|| ft_strchr(*temp, '\0') == -1))
 	{
+		verifier = 1;
 		*b_read = read(fd, buff, BUFFER_SIZE);
 		if (*b_read < 0 || BUFFER_SIZE < *b_read)
 		{
@@ -82,9 +83,9 @@ int	read_file(int fd, char *buff, char **temp, int *b_read)
 			temp_buff = ft_strjoin(*temp, buff, BUFFER_SIZE + 1);
 			*temp = temp_buff;
 		}
-		if (BUFFER_SIZE > *b_read)
-			*b_read = 0;
 	}
+	if (verifier == 0 && ft_strchr(*temp, '\0') == -1)
+		b_read = 0;
 	free(buff);
 	return (1);
 }

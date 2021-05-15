b85f16938d14356aee28368fe687d64b32c48114
Ana Novaes <ana.novaes@cloudwalk.io>
2021-05-15 17:26:55 -0300

    corrigindo leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,15 @@
 /*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 19:50:53 by apaula-b          #+#    #+#             */
-/*   Updated: 2021/05/15 14:34:17 by apaula-b         ###   ########.fr       */
+/*   Updated: 2021/05/15 17:24:10 by apaula-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 #include "get_next_line.h"
+
+
 
 void	*ft_calloc(size_t count, size_t size)
 {
@@ -36,40 +38,39 @@ void	*ft_calloc(size_t count, size_t size)
 	return (pointer);
 }
 
-static int	new_line(char **next_line, char **temp)
+static char	*new_line(char **next_line, char **temp)
 {
 	int		end_line;
 	char	*new_temp;
-	int		size_temp;
 
-	size_temp = ft_strlen(*temp);
+	new_temp = NULL;
 	end_line = ft_strchr(*temp, '\n');
-	if (end_line == -1)
-		end_line = ft_strchr(*temp, '\0');
-	temp[0][end_line] = '\0';
-	*next_line = ft_strdup(*temp);
-	if (!next_line)
-		return (0);
-	if (size_temp > end_line)
+	if (end_line != -1)
 	{
+		temp[0][end_line] = '\0';
+		*next_line = ft_strdup(*temp);
 		end_line++;
 		new_temp = ft_strdup(*temp + end_line);
-		free(*temp);
-		*temp = new_temp;
 	}
 	else
-		*temp = ft_strdup("");
-	return (1);
+		*next_line = ft_strdup(*temp);
+	free(*temp);
+	temp = NULL;
+	if (!next_line || !new_temp)
+		return (NULL);
+	return (new_temp);
 }
 
 int	read_file(int fd, char *buff, char **temp, int *b_read)
 {
 	char	*temp_buff;
+	int		verifier;
 
-	temp_buff = ft_strdup("");
+	verifier = 0;
 	while (*b_read && (ft_strchr(*temp, '\n') == -1
 			|| ft_strchr(*temp, '\0') == -1))
 	{
+		verifier = 1;
 		*b_read = read(fd, buff, BUFFER_SIZE);
 		if (*b_read < 0 || BUFFER_SIZE < *b_read)
 		{
@@ -78,10 +79,13 @@ int	read_file(int fd, char *buff, char **temp, int *b_read)
 		}
 		if (*b_read)
 		{
+			temp_buff = NULL;
 			temp_buff = ft_strjoin(*temp, buff, BUFFER_SIZE + 1);
 			*temp = temp_buff;
 		}
 	}
+	if (verifier == 0 && ft_strchr(*temp, '\0') == -1)
+		b_read = 0;
 	free(buff);
 	return (1);
 }
@@ -104,11 +108,13 @@ int	get_next_line(int fd, char **line)
 	readed = read_file(fd, buff, &temp, &b_read);
 	if (readed == -1)
 		return (-1);
-	 new_line(line, &temp);
+	 temp = new_line(line, &temp);
 	if (!line)
 		return (-1);
 	if (!b_read)
+	{
+		free(temp);
 		return (0);
+	}
 	return (1);
 }
-
